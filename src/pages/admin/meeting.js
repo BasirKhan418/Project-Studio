84dad8e4a2539b53bd8c7d36ad982c55da4c5f24
Basir Khan <khanbasir5555@gmail.com>
2023-08-03 10:09:27 +0530

    optimised for small devices video meeting

--- src/pages/admin/meeting.js
+++ src/pages/admin/meeting.js
@@ -210,7 +210,7 @@ const Meeting = () => {
    
     </div>
     <section className="bg-white dark:bg-gray-900">
-    <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <div className="grid max-w-screen-xl px-4 py-4 mx-auto lg:gap-8 xl:gap-0 lg:py-4 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Unite & Collaborate: Empowering Seamless Group Meetings!</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Premium video meetings now free for every one. Secure Meetings. Create one to one now, Group meeting ,Presentation and live chat etc.</p>
@@ -218,12 +218,12 @@ const Meeting = () => {
                 <FiVideo className='mx-2'/>Create New Meeting
                 <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </button>
-            <input type="text" placeholder='Enter Your Room Id' className='mx-2 border-2 border-blue-600 rounded p-2 font-semibold' name='roomid' value={roomidinput} onChange={handleChange}/>
-            <button  onClick ={joinRoom} className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-600 border border-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <input type="text" placeholder='Enter Your Room Id' className='my-4 mx-2 border-2 border-blue-600 rounded p-2 font-semibold' name='roomid' value={roomidinput} onChange={handleChange}/>
+            <button  onClick ={joinRoom} className="my-0 inline-flex items-center justify-center px-5 py-3 mr-5 text-base font-medium text-center text-white bg-blue-600 border border-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                 Join Now
             </button> 
         </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <div className="lg:mt-0 lg:col-span-5 lg:flex">
             <img src="/video.jpg" alt="mockup"/>
         </div>                
     </div>
