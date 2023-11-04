dd704cbed793dd40fddbdf35a8d7faec0cacd384
Basir Khan <khanbasir5555@gmail.com>
2023-11-04 11:21:54 +0530

    login fixed

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -4,11 +4,11 @@ import connectDb from "../../middleware/mongoose";
 import User from "../../../models/User";
 const handler = async (req, res) => {
   if (req.method == "POST") {
-  try{
     let user= await User.findOne({"email":req.body.email});
-    const bytes  = CryptoJS.AES.decrypt(user.password, process.env.AES_SECRET);
-    const decryptpass = bytes.toString(CryptoJS.enc.Utf8);
+    
     if(user){
+      const bytes  = CryptoJS.AES.decrypt(user.password, process.env.AES_SECRET);
+    const decryptpass = bytes.toString(CryptoJS.enc.Utf8);
         if(req.body.email==user.email&&req.body.password==decryptpass){ 
           const token = jwt.sign({email:user.email,name:user.name}, process.env.JWT_SECRET,);
     res.status(200).json({success: true,token,email:user.email});
@@ -20,10 +20,6 @@ res.status(200).json({ success: false,message:"Invalid credentials"});
 }
 else{
     res.status(200).json({ success: false,message:"No user found"});
-}
-}
-catch(err){
-  res.status(200).json({ success: false,message:"Something went wrong please try again later. Internal server error"});
 }
   }
    else{
