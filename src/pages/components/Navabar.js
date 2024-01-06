5a8257fc3356668ebdf8ecd732d8bb17e8a4e533
vrun1208 <vrun4750@gmail.com>
2024-01-07 02:02:47 +0530

    fix: navbar and hero section styling

--- src/pages/components/Navabar.js
+++ src/pages/components/Navabar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
       <Toaster/>
       <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm sm:h-10 lg:h-24">
         <nav
-          className="mt-6 backdrop-blur-lg relative max-w-7xl w-full border border-gray-200 rounded-[36px] mx-2 py-3 px-4 md:flex md:items-center md:justify-between md:py-0 md:px-6 lg:px-8 xl:mx-auto bg-none"
+          className="mt-6 backdrop-blur-lg relative max-w-7xl w-full border border-gray-200 rounded-[36px] mx-2 py-3 px-4 md:flex md:items-center md:justify-between md:py-7 md:px-6 lg:px-8 xl:mx-auto bg-none"
           aria-label="Global"
         >
           <div className="flex items-center justify-between ">
