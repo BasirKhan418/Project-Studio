61756f2a3db19a988a7705a57330f502e89dce88
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-10-26 23:35:56 +0530

    all done

--- src/pages/admin/Projects.js
+++ src/pages/admin/Projects.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import theme from "../../../trc/theme/theme";
 import FullLayout from "../../../trc/layouts/FullLayout";
 import { ThemeProvider } from "@mui/material/styles";
-import ComingSoon from './ComingSoon';
+
+
 const Projects = () => {
+   useEffect(() => {
+    window.open("https://mbpaf8pcar8dm.pit-1.try.coder.app/workspaces", "_blank");
+  }, []);
   return (
       <ThemeProvider theme={theme}>
        <FullLayout>
@@ -15,7 +19,7 @@ const Projects = () => {
           display:none;
         }
       `}</style>
-        <ComingSoon/>
+        <p>Redirecting...</p>
        </FullLayout>
       </ThemeProvider> 
  
